b4c951fcab913867ee8a747eb3a04003b52ff836
asidanal <asidanal@din51002532.corp.capgemini.com>
2017-09-29 12:33:49 +0530

    changed package name

---
+++ ExhibitMonitor/src/com/exhibit/main/ExhibitMonitorApplication.java
@@ -0,0 +1,27 @@
+package com.exhibit.main;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import com.exhibit.helper.ExhibitMonitorContext;
+import com.exhibit.threads.FileProcessingThread;
+
+public class ExhibitMonitorApplication {
+	
+	public static void main(String[] args) {
+		ExhibitMonitorContext monitorContext = null;
+		
+		try {
+			monitorContext = ExhibitMonitorContext.loadExhibitMonitorContext();
+		} catch (SAXException | IOException | ParserConfigurationException e) {
+			System.out.println("Exception occurred while loading application context.\n"+e.getMessage());
+		}
+	
+		Thread processingThread = new Thread(new FileProcessingThread());
+		processingThread.run();
+	}
+
+}
